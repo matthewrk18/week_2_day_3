daaf393a736d9656d28017cd895b1d5ad0cee013
matthewrk18 <matthewrk18@gmail.com>
2021-09-30 10:32:34 -0400

    first

---
+++ my_module.py
@@ -0,0 +1,6 @@
+def printArea(area):
+    l * w = area
+    l = input('What is the length of your house?')
+    w = input('What is the width of your house?')
+
+    print(f"Your house is {area} square feet")
